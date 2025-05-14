6e20fdfb2f71e1043f13a5f66a9e21bb29140c32
evoke_in_1037 <806048298@qq.com>
2025-05-14 13:10:44 -0700

    report_token

--- backend/utils/tokenUtils.js
+++ backend/utils/tokenUtils.js
@@ -1,13 +1,13 @@
 import jwt from 'jsonwebtoken';
 
 export const generateRememberMeToken = (user) => {
-  return jwt.sign({ id: user.id, email: user.email, icon: user.icon, name: user.name, admin: user.is_admin }, process.env.JWT_SECRET, {
+  return jwt.sign({ id: user.id, email: user.email, icon: user.icon, name: user.name, admin: user.is_admin, report: user.can_report }, process.env.JWT_SECRET, {
     expiresIn: '30d', // longer expiration for remember me
   });
 };
 
 export const generateToken = (user) => {
-    return jwt.sign({ id: user.id, email: user.email, icon: user.icon, name: user.name, admin: user.is_admin }, process.env.JWT_SECRET, {
-      expiresIn: '1h',
-    });
-  };
+  return jwt.sign({ id: user.id, email: user.email, icon: user.icon, name: user.name, admin: user.is_admin, report: user.can_report }, process.env.JWT_SECRET, {
+    expiresIn: '1h',
+  });
+};
